2e8b9c74f003c8056a2e5f1ed0bf5749e3a34089
Jakub Kubala <j.kubala92@gmail.com>
2023-02-27 12:56:11 +0100

    MyAccountPage: locate elements and register user Add LoggedUserPage

--- src/test/java/kubala/tests/RegisterTest.java
+++ src/test/java/kubala/tests/RegisterTest.java
@@ -7,6 +7,7 @@ public class RegisterTest extends BaseTest{
 
     @Test
     public void registerUserTest() {
-        new HomePage(driver).openMyAccountPage();
+        new HomePage(driver).openMyAccountPage()
+                .registerUser("Kijaniatest@test.pl", "Kijaniatest@test.pl");
     }
 }
